5d959daaa3b8f041e3b23a03a37742f1eeec45d9
bibiviolet2 <bibiviolet@gmail.com>
2025-03-15 04:10:17 +0100

    cors

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ import resolvers from './graphql/resolvers/index.js';
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  cors: {
+    origin: ['http://localhost:3000', 'https://sakuraonline.cz', 'https://sakura-8023.rostiapp.cz'], // 🔸 Doplněno podle potřeby
+    credentials: true, // Použij, pokud potřebuješ cookies
+  },
 });
 
 server.listen().then(({ url }) => {
