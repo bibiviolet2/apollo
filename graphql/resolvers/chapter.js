e3867e88cfe3a93ce9a6a22848565d9db1990ce1
bibiviolet2 <bibiviolet@gmail.com>
2025-03-17 04:34:26 +0100

    chapters

--- graphql/resolvers/chapter.js
+++ graphql/resolvers/chapter.js
@@ -3,7 +3,7 @@ import db from '../../db/connection.js';
 const chapterResolvers = {
   Query: {
     chapters: async (_, { book }) => {
-		const [rows] = await db.query('SELECT * FROM chapter WHERE book = ? ORDER BY order ASC', [book]);
+		const [rows] = await db.query('SELECT * FROM chapter WHERE book = ? ORDER BY `order` ASC', [book]);
 		return { results: rows };
 	  },
     chapter: async (_, { slug }) => {
